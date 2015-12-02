ec5c010c6e50a21e712a6f4a6397500463929c6f
Gwangwoon <gksrhkddns@naver.com>
2015-12-02 10:23:56 +0900

    init

---
+++ app/src/main/java/com/example/gwangwoon/themoment/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.gwangwoon.themoment;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Uri mImageCaptureUri;
+    private ImageView mPhotoImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton cameraButton =(ImageButton) findViewById(R.id.camerabutton);
+        cameraButton.setOnClickListener(this);
+        ImageButton videoButton =(ImageButton) findViewById(R.id.videocamerabutton);
+        //cameraButton.setOnClickListener(this);
+        ImageButton pencilButton =(ImageButton) findViewById(R.id.pencilbutton);
+        //cameraButton.setOnClickListener(this);
+        ImageButton recordingButton =(ImageButton) findViewById(R.id.recordingbutton);
+        //cameraButton.setOnClickListener(this);
+
+    }
+
+    private void doTakePhotoAction()
+    {
+    /*
+     * 참고 해볼곳
+     * http://2009.hfoss.org/Tutorial:Camera_and_Gallery_Demo
+     * http://stackoverflow.com/questions/1050297/how-to-get-the-url-of-the-captured-image
+     * http://www.damonkohler.com/2009/02/android-recipes.html
+     * http://www.firstclown.us/tag/android/
+     */
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        // 특정기기에서 사진을 저장못하는 문제가 있어 다음을 주석처리 합니다.
+        //intent.putExtra("return-data", true);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    /**
+     * 앨범에서 이미지 가져오기
+     */
+    private void doTakeAlbumAction()
+    {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+
+
+
+
+
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.camerabutton:
+                DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        doTakePhotoAction();
+                    }
+                };
+
+                DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        doTakeAlbumAction();
+                    }
+                };
+
+                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.dismiss();
+                    }
+                };
+
+                new AlertDialog.Builder(this)
+                        .setTitle("업로드할 이미지 선택")
+                        .setPositiveButton("사진촬영", cameraListener)
+                        .setNeutralButton("앨범선택", albumListener)
+                        .setNegativeButton("취소", cancelListener)
+                        .show();
+
+                /*Intent i = new Intent(this, Camera.class);
+                startActivity(i);*/
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/gwangwoon/themoment/temp.java
@@ -0,0 +1,7 @@
+package com.example.gwangwoon.themoment;
+
+/**
+ * Created by Gwangwoon on 2015-11-26.
+ */
+public class temp {
+}
